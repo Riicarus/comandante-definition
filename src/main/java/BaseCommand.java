60bcfd399c6259bd34af4c8cec842d186bfd0f83
skyline-blue <2895054643@qq.com>
2022-11-15 17:11:47 +0800

    Initial commit

---
+++ src/main/java/BaseCommand.java
@@ -0,0 +1,14 @@
+/**
+ * [FEATURE INFO]<br/>
+ * 基础指令抽象类
+ *
+ * @author Skyline
+ * @create 2022-10-28 19:46
+ * @since 1.0
+ */
+public abstract class BaseCommand {
+    /**
+     * 指令定义
+     */
+    public static void defineCommand() {}
+}
